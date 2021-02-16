ba51f46db7e58a5ba0e3d2fdb1ad34dc6237fb13
hikaru4215 <hikaru4215hkr2178ay@yahoo.co.jp>
2021-02-16 11:28:27 +0900

    permissions修正

--- api/permissions.py
+++ api/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 class IsAdminUserOrReadOnly(permissions.IsAdminUser):
-    def has_object_permission(self, request, view):
+    def has_permission(self, request, view):
         is_admin = super().has_permission(request, view)
 
         return request.method in permissions.SAFE_METHODS or is_admin
