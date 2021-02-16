172ef2bedf313ce0c19378bb2fb55e5d6c79ce7e
hikaru4215 <hikaru4215hkr2178ay@yahoo.co.jp>
2021-02-16 11:27:45 +0900

    ページネーション追加(settings)

--- work_django/settings.py
+++ work_django/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 4,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination'
+}
