4e5904c43db108aaf38644c9db6e6eb4c53fc737
hikaru4215 <hikaru4215hkr2178ay@yahoo.co.jp>
2021-02-16 11:24:24 +0900

    view作成

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from api.models import Profile
+from api.permissions import IsAdminUserOrReadOnly
+from api.serializers import ProfileSerializer
 
-# Create your views here.
+
+class ListView(generics.ListAPIView):
+    queryset = Profile.objects.all().order_by('-id')
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+
+
+class DetailView(generics.RetrieveUpdateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
